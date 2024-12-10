9babead97c13ef5ee6fe1e82973a08f0e1327e40
kajalkumawatregexsoftware <kajalkumawat0404@gmail.com>
2024-12-10 13:09:06 +0530

    user input string paragraph

---
+++ basic/StringUser2.java
@@ -0,0 +1,10 @@
+import java.util.*;
+
+public class StringUser2 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("enter paragraph ");
+        String s = sc.nextLine();
+        System.out.println(s);
+    }
+}
