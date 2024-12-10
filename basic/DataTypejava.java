6ae2f6594a9fb1178f6c0eb0881d056fb7a99e66
kajalkumawatregexsoftware <kajalkumawat0404@gmail.com>
2024-12-10 12:14:59 +0530

    data type in java

---
+++ basic/DataTypejava.java
@@ -0,0 +1,32 @@
+public class DataTypejava {
+    public static void main(String[] args) {
+        // + :string operator (string +value add )
+        // data typee :
+        // boolean :true ya false
+        boolean a = true;
+        System.out.println("boolean :" + a);
+
+        // char :alphabets
+        char b = 'o';
+        System.out.println("character : " + b);
+
+        // byte :stored 127 to -128(numbers)
+        byte c = 127;
+        System.out.println("byte " + c);
+
+        // short :32 thousand
+        short d = 896;
+        System.out.println("short " + d);
+
+        // int : number
+        int e = 75;
+        System.out.println("the value of e " + e);
+
+        //long --->self 
+
+        float f=675.3f;//float f :in java decimal value by default stored in double 
+        System.out.println("float :"+f);
+
+        //double 
+    }
+}
