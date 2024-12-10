56a17a19f669ea3742a7bd7ee2dd928cc3972867
kajalkumawatregexsoftware <kajalkumawat0404@gmail.com>
2024-12-10 13:06:28 +0530

    user input of string one word

---
+++ basic/StringUser1.java
@@ -0,0 +1,10 @@
+import java.util.*;
+
+public class StringUser1 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("enter one word");
+        String s = sc.next();
+        System.out.println(s);
+    }
+}
