65effae08a2f8b53c822fe39e19b9d3b296407a6
kajalkumawatregexsoftware <kajalkumawat0404@gmail.com>
2024-12-20 12:19:01 +0530

    parameterised constructor in java

---
+++ oops/encapsulation/ParameterisedConstructor.java
@@ -0,0 +1,12 @@
+class A {
+    // constructor :parameterised constructor
+    A(int a) {
+        System.out.println("the value of a is " + a);
+    }
+}
+
+public class ParameterisedConstructor {
+    public static void main(String[] args) {
+        new A(34);
+    }
+}
