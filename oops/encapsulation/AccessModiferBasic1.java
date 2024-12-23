d664eb9202b58d9ed5198fc269ea022f8e70e08c
kajalkumawatregexsoftware <kajalkumawat0404@gmail.com>
2024-12-23 12:47:50 +0530

    access modifier private does not access one to another

---
+++ oops/encapsulation/AccessModiferBasic1.java
@@ -0,0 +1,10 @@
+class A{
+    private int a=89;
+}
+public class AccessModiferBasic1 {
+    public static void main(String[] args) {
+       A p=new A();
+       System.out.println(p.a);
+     }
+
+}
