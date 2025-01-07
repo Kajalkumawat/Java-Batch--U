81b84e987d5ca47a52e11f18231970a53402dacb
kajalkumawatregexsoftware <kajalkumawat0404@gmail.com>
2025-01-07 12:06:00 +0530

    creation of abstraction in java

---
+++ abstraction/AbstractBasic1.java
@@ -0,0 +1,11 @@
+//how to create abstract class 
+abstract class A{
+    //abstract method :always declare 
+    //abstract method does not define in abstract class 
+    abstract void show();
+}
+public class AbstractBasic1{
+    public public static void main(String[] args) {
+        
+    }
+}
