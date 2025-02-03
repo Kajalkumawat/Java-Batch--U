dd1b4fe102b7563460308a1c000beaba44d93412
kajalkumawatregexsoftware <kajalkumawat0404@gmail.com>
2025-02-03 10:46:04 +0530

    static concept 1

---
+++ static/StaticConcept1.java
@@ -0,0 +1,12 @@
+class A{
+   static int a=8;
+    void show(){
+        System.out.println(a);
+    }
+}
+public class StaticConcept1 {
+    public static void main(String[] args) {
+        A k=new A();
+        k.show();
+    }
+}
