bda78d8976212aac749b4e6ce4dc7a66d69bca9e
kajalkumawatregexsoftware <kajalkumawat0404@gmail.com>
2025-02-03 10:47:18 +0530

    static concept 3

---
+++ static/StaticConcept3.java
@@ -0,0 +1,13 @@
+
+public class StaticConcept3 {
+    public static void main(String[] abhishek){
+        A k=new A();
+        k.show();
+    }
+}
+class A{
+     int a=8;
+    static void show(){
+         System.out.println(a);
+     }
+ }
