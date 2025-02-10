d516bcb873b140a77c580cb622a9c9adb57c979e
kajalkumawatregexsoftware <kajalkumawat0404@gmail.com>
2025-02-10 10:57:38 +0530

    print forward direction

---
+++ array/ArrayBasic3.java
@@ -0,0 +1,9 @@
+public class ArrayBasic3 {
+    public static void main(String[] args) {
+        int[] a={67,89,46,23};
+       //loop through 
+       for(int i=0;i<a.length;i++){
+        System.out.print(a[i]+" ");
+       }
+    }
+}
