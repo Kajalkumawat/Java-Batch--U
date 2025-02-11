0822205f8c810e021a455c25397b2683d0214a49
kajalkumawatregexsoftware <kajalkumawat0404@gmail.com>
2025-02-11 11:22:39 +0530

    maximum in array

---
+++ array/ArrayBasic12.java
@@ -0,0 +1,13 @@
+public class ArrayBasic12 {
+ public static void main(String[] args) {
+    int a[]={3,4,5,2,1};
+    int max=a[0];
+    for(int i=0;i<a.length;i++){
+        if(a[i]>max){
+            //value update :
+            max=a[i];
+        }
+    }
+    System.out.println(max);
+ }   
+}
