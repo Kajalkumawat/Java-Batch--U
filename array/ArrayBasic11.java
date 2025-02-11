db4095f5b08c606e65abf7c28c986933e3612ae7
kajalkumawatregexsoftware <kajalkumawat0404@gmail.com>
2025-02-11 11:07:04 +0530

    target number

---
+++ array/ArrayBasic11.java
@@ -0,0 +1,11 @@
+public class ArrayBasic11 {
+    public static void main(String[] args) {
+        int a[]={1,2,3,4,5};
+        int target=3;
+        for(int i=0;i<a.length;i++){
+            if(a[i]==target){
+                System.out.println("presnt");
+            }
+        }
+    }
+}
